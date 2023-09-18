5c54c577f2358f1d7b992dd0331dc00899304a83
akash0651 <55975966+akash0651@users.noreply.github.com>
2023-09-18 17:44:24 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
+// Define routes and associated controllers
 
+// Home route
+router.get('/', homeController.home);
 
-
-// setv routes
-router.get('/',homeController.home);
+// User-related routes (under '/users')
 router.use('/users', require('./users'));
+
+// Project-related routes (under '/project')
 router.use('/project', require('./projects'));
-router.use('/issue',require('./issues'))
-module.exports = router; 
+
+// Issue-related routes (under '/issue')
+router.use('/issue', require('./issues'));
+
+module.exports = router;
