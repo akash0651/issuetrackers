a5a7a54d73c207448b428783144c8b1d0815120b
akash0651 <55975966+akash0651@users.noreply.github.com>
2023-09-18 17:46:54 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,22 +3,30 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 const passport = require('passport');
 
+// Define routes and associated controllers
 
-router.get('/sign-up',usersController.signup);
-router.get('/sign-in',usersController.signin);
-router.get('/profile',passport.checkAuthentication,usersController.profile);
-router.post('/create',usersController.create);
+// Sign-up route
+router.get('/sign-up', usersController.signup);
 
+// Sign-in route
+router.get('/sign-in', usersController.signin);
 
+// Profile route (requires authentication)
+router.get('/profile', passport.checkAuthentication, usersController.profile);
+
+// Create a new user
+router.post('/create', usersController.create);
+
+// Create a session (authentication)
 router.post('/create-session', passport.authenticate(
     'local', { failureRedirect: '/user/sign-in' }
 ), usersController.createSession);
 
+// Sign out route
+router.get('/sign-out', usersController.destroySession);
 
-router.get('/sign-out',usersController.destroySession);
-
-router.get('/auth/google', passport.authenticate('google', {scope: ['profile','email']}));
-router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: 'users/sign-in'}), usersController.createSession);
-
+// Google OAuth2 authentication routes
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: 'users/sign-in' }), usersController.createSession);
 
 module.exports = router;
