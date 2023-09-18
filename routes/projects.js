5f1ed5dd09b5e0aeb4815cba3c696fa8de84c2d1
akash0651 <55975966+akash0651@users.noreply.github.com>
2023-09-18 17:45:49 +0530

    Update projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -3,9 +3,18 @@ const router = express.Router();
 const projectController = require('../controllers/project_controller');
 const passport = require('passport');
 
-router.post('/create',passport.checkAuthentication,projectController.create);
-router.get('/open-project/:id',passport.checkAuthentication,projectController.openProject);
-router.get('/destroy/:id',projectController.destroy);
-router.post('/createIssue',passport.checkAuthentication,projectController.createIssue);
+// Define routes and associated controllers
+
+// Create a new project
+router.post('/create', passport.checkAuthentication, projectController.create);
+
+// Open a project by ID
+router.get('/open-project/:id', passport.checkAuthentication, projectController.openProject);
+
+// Destroy (delete) a project by ID
+router.get('/destroy/:id', projectController.destroy);
+
+// Create a new issue for a project
+router.post('/createIssue', passport.checkAuthentication, projectController.createIssue);
 
 module.exports = router;
