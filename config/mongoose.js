ac83703d61d97cf9c9c1071093f7630351dddcd9
akash0651 <55975966+akash0651@users.noreply.github.com>
2023-09-18 17:39:54 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,19 @@
+// Import the Mongoose library for MongoDB connectivity
 const mongoose = require('mongoose');
+
+// Connect to the MongoDB Atlas cluster using the provided connection string
 mongoose.connect(`mongodb+srv://kumarakashsahu98765:KrhlXh2AGON99ZPI@cluster0.dzyqlwv.mongodb.net/issuetrackerdb`);
 
+// Get a reference to the default Mongoose connection
 const db = mongoose.connection;
 
+// Handle errors that may occur during database connection
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
-
+// Once the connection is successfully established, execute this callback
 db.once('open', function(){
     console.log('Connected to Database :: MongoDB');
 });
 
-
+// Export the Mongoose connection object for use in other parts of the application
 module.exports = db;
